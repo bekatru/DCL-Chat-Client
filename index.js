883c62ab7f95e350a4459e0bed4b2499b8d67506
bekatru <teamusicband@gmail.com>
2021-03-07 19:28:48 +0600

    finally

--- index.js
+++ index.js
@@ -1,104 +1,90 @@
-const {handleMessage} = require('./messageHandling');
+const { handleConnection, genFilePath } = require("./messageHandling");
 
 const Client = (room) => {
-    const fs = require("fs");
-    require("dotenv").config();
-    const WebSocketClient = require("websocket").client;
-    // Chat Room IDs
-    const {
-        ROOM_ROOT,
-        ROOM_SURREAL,
-        ROOM_MEHAKJAIN,
-        ROOM_HPRIVAKOS,
-    } = process.env;
-    // Chat Room Names
-    const channel = {
-        id: room,
-        path:
-            room === ROOM_ROOT
-                ? "root"
-                : room === ROOM_SURREAL
-                    ? "surreal"
-                    : room === ROOM_MEHAKJAIN
-                        ? "mehakjain"
-                        : room === ROOM_HPRIVAKOS
-                            ? "hprivakos"
-                            : "other",
-    };
-    // Create Logfile Path
-    function setFilePath() {
-        const date = new Date()
-            .toLocaleString("en-US", {
-                month: "2-digit",
-                day: "2-digit",
-                year: "numeric",
-            })
-            .split("/")
-            .join("-");
-        return `../chatlogs/${channel.path}/${channel.path}_${date}.json`;
-    }
-    // Write Message to Logfile
+  const fs = require("fs");
+  require("dotenv").config();
+  const WebSocketClient = require("websocket").client;
+  // Chat Room IDs
+  const {
+    ROOM_ROOT,
+    ROOM_SURREAL,
+    ROOM_MEHAKJAIN,
+    ROOM_HPRIVAKOS,
+  } = process.env;
+  // Chat Room Names
+  const channel = {
+    id: room,
+    path:
+      room === ROOM_ROOT
+        ? "root"
+        : room === ROOM_SURREAL
+        ? "surreal"
+        : room === ROOM_MEHAKJAIN
+        ? "mehakjain"
+        : room === ROOM_HPRIVAKOS
+        ? "hprivakos"
+        : "other",
+  };
+  // Write Message to Logfile
 
-    function logMessage(message) {
-        const path = setFilePath();
-        try {
-            if (fs.existsSync(path)) {
-                fs.readFile(path, "utf8", function readFileCallback(err, data) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        const log = JSON.parse(data);
-                        log.messages.push(JSON.parse(message.utf8Data));
-                        const json = JSON.stringify(log, null, 2);
-                        fs.writeFile(path, json, "utf8", () => { });
-                    }
-                });
-            } else {
-                fs.writeFile(
-                    path,
-                    JSON.stringify(
-                        {
-                            info: { room: channel.path, created: new Date() },
-                            messages: [JSON.parse(message.utf8Data)],
-                        },
-                        null,
-                        2
-                    ),
-                    () => console.log("New Log File Created")
-                );
-            }
-        } catch (err) {
+  function logMessage(message) {
+    const path = genFilePath(channel.path);
+    try {
+      if (fs.existsSync(path)) {
+        fs.readFile(path, "utf8", function readFileCallback(err, data) {
+          if (err) {
             console.log(err);
-        }
+          } else {
+            const log = JSON.parse(data);
+            log.messages.push(JSON.parse(message.utf8Data));
+            const json = JSON.stringify(log, null, 2);
+            fs.writeFile(path, json, "utf8", () => {});
+          }
+        });
+      } else {
+        fs.writeFile(
+          path,
+          JSON.stringify(
+            {
+              info: { room: channel.path, created: new Date() },
+              messages: [JSON.parse(message.utf8Data)],
+            },
+            null,
+            2
+          ),
+          () => console.log("New Log File Created")
+        );
+      }
+    } catch (err) {
+      console.log(err);
     }
-    
-    // Create Instance of WS
-    const client = new WebSocketClient();
-    client.on("connectFailed", function (error) {
-        console.log("Connect Error: " + error.toString());
+  }
+
+  // Create Instance of WS
+  const client = new WebSocketClient();
+  client.on("connectFailed", function (error) {
+    console.log("Connect Error: " + error.toString());
+  });
+  // Handle Connection Open
+  client.on("connect", function (connection) {
+    console.log("WebSocket Client Connected");
+    connection.on("error", function (error) {
+      console.log("Connection Error: " + error.toString());
     });
-    // Handle Connection Open
-    client.on("connect", function (connection) {
-        console.log("WebSocket Client Connected");
-        connection.on("error", function (error) {
-            console.log("Connection Error: " + error.toString());
-        });
-        // Handle Connection Close
-        connection.on("close", function () {
-            console.log("Connection Closed");
-        });
-        //Handle Messages
-        connection.on("message", function (message) {
-            /////////////////////////
-            connection.send(handleMessage(message.utf8Data));
-            //     logMessage(message);
-            /////////////////////////
-        });
+    // Handle Connection Close
+    connection.on("close", function () {
+      console.log("Connection Closed");
     });
-    // Connect to WS
-    client.connect(
-        `wss://us-nyc-1.websocket.me/v3/${room}?api_key=${process.env.BEKA_PS_API}`
-    );
+    //Handle Messages
+    connection.on("message", function (message) {
+      const send = (data) => connection.send(data);
+      handleConnection(message.utf8Data, channel.path, send);
+    });
+  });
+  // Connect to WS
+  client.connect(
+    `wss://us-nyc-1.websocket.me/v3/${room}?api_key=${process.env.BEKA_PS_API}`
+  );
 };
 
 exports.Client = Client;
--- messageHandling.js
+++ messageHandling.js
@@ -1,19 +1,133 @@
+const fs = require("fs");
+const util = require("util");
+
+const n = 5; //Number of last messages
+const m = 15; //Number of minutes
 const onlineUsers = [];
 
-const handleMessage = (data) => {
-    const {type, message, name, id} = JSON.parse(data);
-    switch (type + '_' + message) {
-        case 'connection_joined':
-            onlineUsers.push({id, name});
-            break;
-        case 'connection_left':
-            const i = onlineUsers.findIndex((user) => user.id === id);
-            onlineUsers.splice(i, 1);
-            break;
-        default:
-            console.log(data);
-    }
-    return JSON.stringify({id: 'service', type, onlineUsers});
+const handleConnection = (data, room, callback) => {
+  const { type, message, name, id } = JSON.parse(data);
+  switch (type + "_" + message) {
+    case "connection_joined":
+      onlineUsers.push({ id, name });
+      getRecentMessages(room, n, m).then((data) => {
+        if (data) {
+          callback(
+            JSON.stringify({
+              id: "service",
+              type,
+              onlineUsers,
+              messages: data,
+              adress: id,
+            })
+          );
+        }
+      });
+      break;
+    case "connection_left":
+      const i = onlineUsers.findIndex((user) => user.id === id);
+      onlineUsers.splice(i, 1);
+      callback(
+        JSON.stringify({
+          id: "service",
+          type,
+          onlineUsers,
+        })
+      );
+      break;
+    default:
+      logMessage(room, JSON.parse(data));
+  }
 };
 
-exports.handleMessage = handleMessage;
+// Get Recent Messages
+const getRecentMessages = (room, number, minutes) => {
+  return getLogMessages(room)
+    .then((data) => JSON.parse(data))
+    .then(({ messages }) => {
+      const last = messages.slice(Math.max(messages.length - number, 1));
+      const recent = last.filter(
+        ({ date }) =>
+          new Date(date).getTime() > new Date().getTime() - minutes * 60000
+      );
+      return recent;
+    })
+    .catch((err) => console.error(err));
+};
+
+//Log Message
+const logMessage = (room, message) => {
+  getLogMessages(room)
+    .then((data) => JSON.parse(data))
+    .then(({ info, messages }) => {
+      fs.writeFile(
+        genFilePath(room),
+        JSON.stringify(
+          {
+            info,
+            messages: [...messages, message],
+          },
+          null,
+          2
+        ),
+        (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("Message Logged");
+          }
+        }
+      );
+    })
+    .catch((error) => {
+      switch (error.code) {
+        case "ENOENT":
+          console.log(
+            "!!!Error!!!\ngetLastMessages()\nFile doesn't exist\n" + file
+          );
+          createLogFile(room);
+          break;
+        default:
+          console.error(error);
+      }
+    });
+};
+
+//Get Log Messages
+const getLogMessages = (room) => {
+  const read = util.promisify(fs.readFile);
+  const file = genFilePath(room);
+  return read(file, "utf8");
+};
+
+//Create Log File
+const createLogFile = (room) => {
+  fs.writeFile(
+    genFilePath(room),
+    JSON.stringify(
+      {
+        info: { room, created: new Date() },
+        messages: [],
+      },
+      null,
+      2
+    ),
+    () => console.log("New Log File Created")
+  );
+};
+
+//Generate File Path
+const genFilePath = (room) => {
+  const date = new Date()
+    .toLocaleString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    })
+    .split("/")
+    .join("-");
+  return `../chatlogs/${room}/${room}_${date}.json`;
+};
+
+exports.genFilePath = genFilePath;
+exports.handleConnection = handleConnection;
